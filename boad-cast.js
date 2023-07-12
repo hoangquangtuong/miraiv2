9f055de59ed35c21d313878b6058d074a44fd528
Hoàng Quang Tường <tuong.hoangquang2004@hcmut.edu.vn>
2023-07-12 09:18:59 +0700

    boad cast

---
+++ boad-cast.js
@@ -0,0 +1,9 @@
+[
+   "xin chào chủ nhân :>",
+   "liên hệ fb: https://www.facebook.com/copyrightQuangTuong.Developers",
+   "Xin chào cậu chủ Hoàng Quang Tường ạ",
+   "Chào mừng cậu chủ trở lại",
+   "Chào cậu chủ Quang Tường đẹp trai UwU",
+   "Chúc cậu chủ có một ngày tốt lành nhéee",
+   "Chúc một ngày vui vẻ nhé chủ nhân đẹp trai UwU"
+]
